311cdad2c2b76a0dfdbeeb3a6c10c04cdb0b966a
Yves Senn <yves.senn@gmail.com>
2012-09-01 15:56:19 +0200

    Hash#deep_merge is no longer required

--- lib/corner_stones/table.rb
+++ lib/corner_stones/table.rb
@@ -2,19 +2,6 @@ require 'corner_stones/table/selectable_rows'
 require 'corner_stones/table/deletable_rows'
 require 'corner_stones/table/whitespace_filter'
 
-class Hash
-  def deep_merge(other_hash)
-    dup.deep_merge!(other_hash)
-  end
-  def deep_merge!(other_hash)
-    other_hash.each_pair do |k,v|
-      tv = self[k]
-      self[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.deep_merge(v) : v
-    end
-    self
-  end
-end
-
 module CornerStones
 
   class Table
